7879f3523f7d98138fc0b2a288dee0da0d2a846e
Carlos Tavares <carlostsa10@gmail.com>
2022-05-20 13:55:53 -0300

    main stuff

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MainRoutes from './src/routes';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> 
-    <MainRoutes />
+    <BrowserRouter>
+      <MainRoutes />
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,13 @@ import Main from './pages/Main';
 import SignIn from './pages/SignIn';
 
 function MainRoutes() {
-    
+
     return (
         <Routes>
+            <Route path='/' element={Main} />
             <Route path="/singin" element={SignIn} />
         </Routes>
     )
 }
+
+export default MainRoutes
